40f09063cb8091641450b36230aaaa4b6719b48d
Paul Martin <Dmart331@gmail.com>
2016-10-13 13:49:57 -0500

    finished object loop

--- for-loops.js
+++ for-loops.js
@@ -1,11 +1,19 @@
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
 
-var pres = ["George Washington" , "Thomas Jefferson" , "Bill Clinton" , "George Bush" , "Barack Obama"];
-var order = ["president # 1 is " , "president # 2 is " , "president # 3 is " , "president # 4 is " , "president # 5 is "]
-
-for (var i = 0; i < order.length; i++) {
-	var temp = order[i];
-	var temp2 = pres[i]; 
-		console.log(temp + temp2)
+for (var prop in antSpecies) {
+		console.log("antSpecies :" + prop );
 	}
 	
 	
