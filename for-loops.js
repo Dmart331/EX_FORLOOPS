11202fade091f9241f97bec915e9badc02b4c299
Paul Martin <Dmart331@gmail.com>
2016-10-13 11:47:23 -0500

    trying to get console to print result of 2 strings at once

--- for-loops.js
+++ for-loops.js
@@ -1,4 +1,14 @@
-for (var i = 4096; i >= 1; i = i / 2) {
-	console.log(i);
+
+var pres = ["The first president was George Washington" , "I think Thomas Jefferson was the second" , "Bill Clinton... you get the idea" , "I remember George Bush" , "Barack Obama isn't as bad as i thought"];
+var order = ["president # 1 is " , "president # 2 is " , "president # 3 is " , "president # 4 is " , "president # 5 is "]
+
+for (var i = 0; i < order.length; i++) {
+
+
+for (var d = 0; d < pres.length; d++) {
+
+
+console.log(order[i] + pres[d]);
 }
 
+}
