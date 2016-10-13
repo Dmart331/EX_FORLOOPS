d90de5e81f8ff98fd3117fd56de79fbd07955899
Paul Martin <Dmart331@gmail.com>
2016-10-13 12:14:25 -0500

    finished array loop

--- for-loops.js
+++ for-loops.js
@@ -1,14 +1,12 @@
 
-var pres = ["The first president was George Washington" , "I think Thomas Jefferson was the second" , "Bill Clinton... you get the idea" , "I remember George Bush" , "Barack Obama isn't as bad as i thought"];
+var pres = ["George Washington" , "Thomas Jefferson" , "Bill Clinton" , "George Bush" , "Barack Obama"];
 var order = ["president # 1 is " , "president # 2 is " , "president # 3 is " , "president # 4 is " , "president # 5 is "]
 
 for (var i = 0; i < order.length; i++) {
+	var temp = order[i];
+	var temp2 = pres[i]; 
+		console.log(temp + temp2)
+	}
+	
+	
 
-
-for (var d = 0; d < pres.length; d++) {
-
-
-console.log(order[i] + pres[d]);
-}
-
-}
